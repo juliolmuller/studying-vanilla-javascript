48f102008421fd1014b028cdb0dbcc1bf07edac4
Julio Muller <juliodolago@gmail.com>
2020-07-28 23:30:26 -0300

    Updated AdminLTE 3 components

--- src/js/index.js
+++
@@ -1,2 +0,0 @@
-
-const user = new UserController('users-table', 'box-user-create', 'box-user-edit')
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-multi-assign
+window.jQuery = window.$ = require('jquery')
+require('bootstrap')
+require('admin-lte')
+// import './controllers/UserController'
+
+// new UserController('users-table', 'create-user-form', 'edit-user-form')
